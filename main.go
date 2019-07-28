f1c6e6cb7177e11a20a5072b64a168d900b41e49
shorii <aozora911107@ezweb.ne.jp>
2019-07-28 14:24:30 +0900

    add dom op

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+	"time"
+)
+
+const (
+	TIME_FORMAT = "2006/01/02 15:04:05"
+)
 
 func main() {
-	fmt.Println("Hello, wasm!")
+	ticker := time.NewTicker(1 * time.Second)
+	body := js.Global().Get("document").Call("querySelector", "body")
+	for {
+		<-ticker.C
+		currentTime := time.Now().Format(TIME_FORMAT)
+		body.Set("innerText", currentTime)
+	}
 }
